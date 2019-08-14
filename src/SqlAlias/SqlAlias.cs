1c79841c8baae3c3823467f2caff7ab4aaee0228
tgunness <tgunness@gmail.com>
2019-08-13 23:55:57 -0400

    updated for net4

--- src/SqlAlias/SqlAlias.cs
+++ src/SqlAlias/SqlAlias.cs
@@ -8,32 +8,24 @@ namespace SqlAlias
     {
         public static string Map(string connectionString)
         {
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) &&
-                RuntimeInformation.FrameworkDescription.StartsWith(".NET Core")) // netstandard library may be used from NetFX runtime
+            try
             {
+                var builder = new SqlConnectionStringBuilder(connectionString);
 
-                try
-                {
-                    var builder = new SqlConnectionStringBuilder(connectionString);
+                var key = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") == "x86"
+                    ?  @"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\MSSQLServer\Client\ConnectTo"
+                    :  @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo";
 
-                    var key = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE") == "x86"
-                        ?  @"HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\MSSQLServer\Client\ConnectTo"
-                        :  @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\MSSQLServer\Client\ConnectTo";
+                var newSource = (string)Microsoft.Win32.Registry.GetValue(key, builder.DataSource, null);
+                if (newSource != null)
+                    builder.DataSource = newSource.Substring(newSource.IndexOf(',') + 1);
 
-                    var newSource = (string)Microsoft.Win32.Registry.GetValue(key, builder.DataSource, null);
-                    if (newSource != null)
-                        builder.DataSource = newSource.Substring(newSource.IndexOf(',') + 1);
-
-                    return builder.ConnectionString;
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception("Failed to map the SQL Server alias", ex);
-                }
+                return builder.ConnectionString;
             }
-
-            return connectionString;
-
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to map the SQL Server alias", ex);
+            }
         }
     }
 }
